d731d8db475378b2192939fc22c3397c09913dfc
vrnranjan22 <vrnranjan22@gmail.com>
2020-04-01 12:04:58 +0530

    profilePic updating done

--- flaskblog/form.py
+++ flaskblog/form.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, length, email, equal_to
 from flaskblog.model import User
-
 
 
 class RegistrationForm(FlaskForm):
@@ -28,3 +29,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), email()])
+    picture = FileField('Update profile Pic', validators=[FileAllowed(['jpeg', 'jpg', 'png', 'svg'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("Username name not available, try different one")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("Email already in use")
